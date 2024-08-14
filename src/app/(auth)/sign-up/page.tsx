9fe08c49ed34fb4299f8edeec7a58776a608202b
Rupoum <rupoummitra15278@gmail.com>
2024-08-14 13:43:33 +0530

    sign up sign in added

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div className="mt-24 rounded-xl bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+      <form action="">
+        <div className="flex justify-center">
+          <h1 className="text-3xl font-semibold text-white ">Signup</h1>
+        </div>
+        <div className="space-y-4 mt-5">
+          <Input
+            type="name"
+            name="name "
+            placeholder="Name"
+            className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"
+          />
+          <Input
+            type="email"
+            name="email "
+            placeholder="Email"
+            className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"
+          />
+
+          <Button
+            variant="destructive"
+            className="bg-[#e50914] text-black w-full "
+          >
+            Sign Up
+          </Button>
+        </div>
+      </form>
+
+      <div className="text-gray-500 text-sm mt-2">
+        Already have an account ?
+        <Link href="/login" className="text-white  ml-1 hover:underline">
+          Log in now !
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default page;
