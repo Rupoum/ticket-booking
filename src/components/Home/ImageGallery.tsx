4aabc6b01a70eedec937e812e7c91b75252450ba
YOUVAL SINGH <106860504+ross2121@users.noreply.github.com>
2024-08-14 22:43:35 +0530

    Update ImageGallery.tsx

--- src/components/Home/ImageGallery.tsx
+++ src/components/Home/ImageGallery.tsx
@@ -2,36 +2,40 @@
 
 import React, { useState } from "react";
 
-const ImageGallery = ({ images }) => {
+const ImageGallery = ({ images }: any) => {
   const [selectedImage, setSelectedImage] = useState(images[0]);
 
   return (
-    <div className="h-[500px] w-screen flex flex-col justify-center items-center">
-      <div className="relative w-full h-2/3 bg-gray-300">
-        <img
-          src={selectedImage}
-          alt="Selected"
-          className="w-full h-full object-cover rounded-lg shadow-lg"
-        />
-      </div>
-
-      <div className="w-full h-1/3 bg-white p-4 mt-4 flex justify-center items-center space-x-4 overflow-x-scroll rounded-lg shadow-lg">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Thumbnail ${index + 1}`}
-            className={`h-full object-cover rounded-lg cursor-pointer transition-transform duration-200 ease-in-out ${
-              selectedImage === image
-                ? "transform scale-110 border-4 border-indigo-500"
-                : "opacity-70 hover:opacity-100"
-            }`}
-            onClick={() => setSelectedImage(image)}
-          />
-        ))}
+    <div className="h-screen w-screen relative">
+      {/* Main Image Section */}
+      <div
+        className="relative w-full h-3/4 bg-black"
+        style={{
+          backgroundImage: `url(${selectedImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        {/* Thumbnails Overlay */}
+        <div className="absolute bottom-0 left-0 w-full h-1/4 p-4 bg-black bg-opacity-50 flex justify-center items-center space-x-4 overflow-x-scroll no-scrollbar">
+          {images.map((image: any, index: any) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Thumbnail ${index + 1}`}
+              className={`h-full object-contain rounded-lg cursor-pointer transition-transform duration-200 ease-in-out ${
+                selectedImage === image
+                  ? "transform scale-110 border-4 border-indigo-500"
+                  : "opacity-70 hover:opacity-100"
+              }`}
+              onMouseEnter={() => setSelectedImage(image)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default ImageGallery;
+
