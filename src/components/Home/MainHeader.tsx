e40aac5c0a36fe4fb21cd48758bf26e94886cc26
Rupoum <rupoummitra15278@gmail.com>
2024-08-16 15:14:11 +0530

    minor error handled

--- src/components/Home/MainHeader.tsx
+++ src/components/Home/MainHeader.tsx
@@ -5,6 +5,7 @@ const MainHeader = () => {
   return (
     <div className="relative flex h-[30rem] w-full flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Image
+        alt="header image"
         src={Background}
         className="hidden sm:flex sm:object-cover -z-10 brightness-50"
         priority
